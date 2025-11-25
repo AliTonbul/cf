2c5bb7c5533866ffd87e845f8364d537485675aa
AliTonbul <alitonbul2@gmail.com>
2025-11-25 14:26:49 +0000

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 import Link from 'next/link'
-
+import { Button } from '@/components/ui/button'
+import { Navbar } from '@/components/Navbar';
 export default async function Home() {
   //const supabase = await createClient()
 
@@ -15,27 +16,17 @@ export default async function Home() {
 
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="px-4 lg:px-6 h-14 flex items-center">
-        <Link className="flex items-center justify-center" href="#">
-          <span className="font-bold text-xl">GeoTrack</span>
-        </Link>
-        <nav className="ml-auto flex gap-4 sm:gap-6">
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/login">
-            Sign In
-          </Link>
-            <Link className="text-sm font-medium hover:underline underline-offset-4" href="/signup">
-            Sign up
-          </Link>
-        </nav>
-      </header>
+      <Navbar />
+      
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-gray-50">
           <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-            Track Remote Teams with Ease
+            Track 
           </h1>
           <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl">
-            Simple, reliable geolocation tracking for your remote workforce. Clock in, track location, and manage timesheets.
+blabl
           </p>
+
         </section>
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
